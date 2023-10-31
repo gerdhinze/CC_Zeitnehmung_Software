4a475bd59f2442bb0950b36ef26a1fd97f642881
gerdhinze <gerd.hinteregger@edu.fh-joanneum.at>
2023-10-31 13:48:29 +0100

    Make it working

--- ESP8266_RFID_MATLAB/ESP8266_RFID_MATLAB/ESP8266_RFID_MATLAB.ino
+++ ESP8266_RFID_MATLAB/ESP8266_RFID_MATLAB/ESP8266_RFID_MATLAB.ino
@@ -1,34 +1,25 @@
-int variableWert = 0; 
 
-void setup() 
+//For ESP8266
+int value;
+void setup()
+
 {
-  Serial.begin(115200); // Starten Sie die serielle Kommunikation mit 115200 Baud
+Serial.begin(9600);
+pinMode(2, OUTPUT);
 }
 
-void loop() 
+void loop()
 {
-  if (Serial.available() > 0) 
+  if(Serial.available()>0)
   {
-    String input = Serial.readStringUntil('\n'); // Liest die Eingabezeile bis zum Zeilenumbruch (\n)
-    input.trim(); // Entfernt führende und abschließende Leerzeichen
-    
-    // Wenn der Befehl "set" eingegeben wurde
-    if (input == "set") 
-    {
-      Serial.println("Bitte geben Sie einen Wert ein:");
-      while (Serial.available() <= 0) 
-      {
-        // Warten Sie, bis etwas im Serial Monitor eingegeben wird
-      }
-      variableWert = Serial.parseInt(); // Liest den eingegebenen Wert und speichert ihn in variableWert
-      Serial.println("Wert wurde gesetzt.");
+    value=Serial.read();
+    if  (value == 1)           
+    { 
+    digitalWrite(2, HIGH);
     }
-    
-    // Wenn der Befehl "get" eingegeben wurde
-    else if (input == "get") 
-    {
-      Serial.print("Aktueller Wert der Variable: ");
-      Serial.println(variableWert);
+    if(value == 0)         
+    { 
+    digitalWrite(2, LOW);
     }
   }
 }
