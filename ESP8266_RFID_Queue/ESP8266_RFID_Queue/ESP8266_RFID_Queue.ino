6a1b900f06f6ef635a71dc0e09712ecd767551fb
gerdhinze <gerd.hinteregger@edu.fh-joanneum.at>
2023-11-01 09:50:13 +0100

    modify the queue example

--- ESP8266_RFID_Queue/ESP8266_RFID_Queue/ESP8266_RFID_Queue.ino
+++ ESP8266_RFID_Queue/ESP8266_RFID_Queue/ESP8266_RFID_Queue.ino
@@ -1,7 +1,9 @@
-
+const int queue_size = 10;
+//Definition of the class
+//########################################################################
 class Queue {
 private:
-    static const int MAX_SIZE = 10;
+    static const int MAX_SIZE = queue_size;
     int elements[MAX_SIZE];
     int front;
     int rear;
@@ -13,27 +15,22 @@ public:
         rear = -1;
         count = 0;
     }
-
     bool isEmpty() {
         return count == 0;
     }
-
     bool isFull() {
         return count == MAX_SIZE;
     }
-
     void enqueue(int item) {
         if (isFull()) {
-            // Wenn die Queue voll ist, entfernen Sie das älteste Element (vorderstes Element).
+            // if queue is full, the last element is removed
             front = (front + 1) % MAX_SIZE;
             count--;
         }
-
         rear = (rear + 1) % MAX_SIZE;
         elements[rear] = item;
         count++;
     }
-
     int dequeue() {
         if (!isEmpty()) {
             int item = elements[front];
@@ -42,10 +39,9 @@ public:
             return item;
         } else {
             Serial.println("Queue is empty. Cannot dequeue.");
-            return -1; // You can return a special value to indicate an empty queue
+            return -1; 
         }
     }
-
     void printQueue() {
         Serial.print("Queue: ");
         for (int i = 0; i < count; i++) {
@@ -56,20 +52,28 @@ public:
         Serial.println();
     }
 };
-
+//#######################################################################
 Queue myQueue;
-
+int count = 0;
 void setup() {
     Serial.begin(115200);
 }
-
 void loop() {
-    // Fügen Sie ständig neue Elemente zur Queue hinzu
-    int newItem = random(9); // Beispiel: Zufällige Zahl zwischen 0 und 99
+    // A new items is added continuously
+    int newItem = random(9); // Random number between 0 and 9
     myQueue.enqueue(newItem);
 
-    // Ausgabe der Queue-Elemente am Serial Monitor
+    Serial.print(count);
+    Serial.print(".:");
+    // Output of the queue in serial monitor
     myQueue.printQueue();
 
-    delay(1000); // Warten Sie für eine Sekunde, bevor Sie ein neues Element hinzufügen
+    delay(1000);
+
+    if(count < queue_size-1){
+      count ++;
+    }
+    else{
+      count = 0;
+    }
 }
