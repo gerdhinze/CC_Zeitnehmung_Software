0346bcbdbea6cce40f017ad7541a50ac69db5701
gerdhinze <gerd.hinteregger@edu.fh-joanneum.at>
2023-11-01 17:20:42 +0100

    implement rx and tx

--- MATLAB_ESP_RX_TX.m
+++ MATLAB_ESP_RX_TX.m
@@ -6,13 +6,22 @@ while true
 
 % Lesen der Daten vom Arduino
 try
-    a = input('Befehl eingeben:');
-    fprintf(esp,a);
-
-    size = 20; % Anpassen der Größe
-    dataReceived = fscanf(esp, '%s', size);
-    disp(dataReceived);
-  
+    % Warten auf Benutzereingabe in der MATLAB-Konsole
+    userInput = input('Befehl eingeben (get/set Wert): ', 's');
+    
+    % Überprüfen, ob der Befehl 'exit' ist, um das Programm zu beenden
+    if strcmpi(userInput, 'exit')
+        break;
+    end
+    
+    % Senden Sie den Befehl an den ESP8266
+    fprintf(esp, userInput);
+    
+    % Warten auf eine Antwort vom ESP8266
+    response = fscanf(esp, '%s');
+    
+    % Zeigen Sie die Antwort in der MATLAB-Konsole an
+    disp(['Antwort vom ESP8266: ' response]);
 catch
     disp('Fehler beim Lesen von Daten vom Arduino.');
    
