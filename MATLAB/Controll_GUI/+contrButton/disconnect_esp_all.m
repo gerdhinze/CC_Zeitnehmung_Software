0b2a2b8dd3f323c90b645ae9c4b941dabf919391
gerdhinze <gerd.hinteregger@edu.fh-joanneum.at>
2023-11-13 10:04:34 +0100

    bufix

--- MATLAB/Controll_GUI/+contrButton/disconnect_esp_all.m
+++ MATLAB/Controll_GUI/+contrButton/disconnect_esp_all.m
@@ -1,13 +1,12 @@
 function disconnect_esp_all()
     if isvalid(esp1)
-    fclose(esp1);
-    delete(esp1);
-    disp(realtime_esp1);
+        fclose(esp1);
+        delete(esp1);
     elseif isvalid(esp2)
-    fclose(esp2);
-    delete(esp2);
+        fclose(esp2);
+        delete(esp2);
     elseif isvalid(esp3)
-    fclose(esp3);
-    delete(esp3);
+        fclose(esp3);
+        delete(esp3);
     end 
 end
--- MATLAB/Controll_GUI/+contrButton/stop.m
+++ MATLAB/Controll_GUI/+contrButton/stop.m
@@ -1,11 +1,11 @@
 function stop()
     
     if isvalid(esp1)
-    fprintf(esp1, 'Q');
+        fprintf(esp1, 'Q');
     elseif isvalid(esp2)
-    fprintf(esp2, 'Q');
+        fprintf(esp2, 'Q');
     elseif isvalid(esp3)
-    fprintf(esp3, 'Q');
+        fprintf(esp3, 'Q');
     end
 end
 
