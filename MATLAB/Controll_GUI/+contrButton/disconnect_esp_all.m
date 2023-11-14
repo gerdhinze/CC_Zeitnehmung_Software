5c62ce5c02c77ed82127cb76c768e66ee8317e56
gerdhinze <gerd.hinteregger@edu.fh-joanneum.at>
2023-11-14 12:27:19 +0100

    bugfixing

--- MATLAB/Controll_GUI/+contrButton/disconnect_esp_all.m
+++ MATLAB/Controll_GUI/+contrButton/disconnect_esp_all.m
@@ -2,17 +2,17 @@ function disconnect_esp_all()
     global esp1;
     global esp2;
     global esp3;
-    if strcmp(get(esp1, 'Status'), 'open')
+    if isvalid(esp1) && isequal(esp1.Status, 'open')
         fclose(esp1);
         delete(esp1);
         clear esp1;
     end
-    if strcmp(get(esp2, 'Status'), 'open')
+    if isvalid(esp2) && isequal(esp2.Status, 'open')
         fclose(esp2);
         delete(esp2);
         clear esp2;
     end
-    if strcmp(get(esp3, 'Status'), 'open')
+    if isvalid(esp3) && isequal(esp3.Status, 'open')
         fclose(esp3);
         delete(esp3);
         clear esp3;
