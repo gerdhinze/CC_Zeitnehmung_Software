5d0c9bb9c5d9170d7178a79a041275a753881cd0
gerdhinze <gerd.hinteregger@edu.fh-joanneum.at>
2023-11-30 10:12:28 +0100

    change serial to serial port

--- MATLAB/Controll_GUI/+contrButton/connect2ESP.m
+++ MATLAB/Controll_GUI/+contrButton/connect2ESP.m
@@ -1,5 +1,6 @@
 function [parity, esp] = connect2ESP(comPort)
-    esp = serial(comPort, 'BAUD', 115200);
+    %esp = serial(comPort, 'BAUD', 115200);
+    esp = serialport(comPort, 115200);
     fopen(esp);
     parity = 1;
 end
