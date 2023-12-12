7fdba271b1ec13653bf5d117046d7582c1bf0507
gerdhinze <gerd.hinteregger@edu.fh-joanneum.at>
2023-12-12 12:01:39 +0100

    making a v2 controll gui

---
+++ MATLAB/Controll_GUI_V2/+contrButton/get_time.m
@@ -0,0 +1,29 @@
+function [time_esp, command, receive] = get_time(esp, parity)
+    % Initialize output variable
+    time_esp = [];
+    receive = '';
+    
+    if parity == 1
+        command = 'get_time';
+        fprintf(esp, command);
+        pause(0.5);
+        while esp.BytesAvailable > 0
+            string_time = fgets(esp);
+
+            if startsWith(string_time, 't')
+                % Eliminate first charakter
+                receive = 'Realtime gesendet';
+                time_esp = extractAfter(string_time, 1);
+                
+            else
+            time_esp = '00:00:00';
+            end
+        end
+    else
+        command = 'Keine Verbindung';
+        receive = 'Keine Verbindung';
+        % Handle the case where parity is not equal to 1
+        time_esp = '00:00:00';
+    end
+end
+
---
+++ MATLAB/Controll_GUI_V2/+contrButton/start.m
@@ -0,0 +1,90 @@
+function  start(txtAreaESP1_tx, txtAreaESP2_tx, txtAreaESP3_tx, txtAreaESP1_rx, txtAreaESP2_rx, txtAreaESP3_rx, dataFile)
+    
+    global esp1 esp2 esp3;
+    global parity1 parity2 parity3;
+    global stop_value;
+
+
+    if parity1 && parity2 && parity3
+        command_esp1 = 'start';
+        command_esp2 = 'start';
+        command_esp3 = 'start';
+
+        txtAreaESP1_tx.Value = 'start';
+        txtAreaESP2_tx.Value = 'start';
+        txtAreaESP3_tx.Value = 'start';
+
+        txtAreaESP1_rx.Value = 'Erkenne Zeiten';
+        txtAreaESP2_rx.Value = 'Erkenne Zeiten';
+        txtAreaESP3_rx.Value = 'Erkenne Zeiten';
+
+        fprintf(esp1, command_esp1);
+        fprintf(esp2, command_esp2);
+        fprintf(esp3, command_esp3);
+
+        while stop_value == 0
+            % Process data from esp1
+            txtAreaESP1_rx.Value = processEspData(esp1, dataFile);
+
+            % Process data from esp2
+            txtAreaESP2_rx.Value = processEspData(esp2, dataFile);
+
+            % Process data from esp3
+            txtAreaESP3_rx.Value = processEspData(esp3, dataFile);
+            drawnow;
+        end
+
+    else
+        txtAreaESP1_rx.Value = 'Bitte alle Stationen verbinden';
+        txtAreaESP2_rx.Value = 'Bitte alle Stationen verbinden';
+        txtAreaESP3_rx.Value = 'Bitte alle Stationen verbinden';
+    end
+end
+
+%##############################################################################
+function [receive] = processEspData(esp, dataFile)
+    receive = '';
+    while esp.BytesAvailable > 0
+        data_esp = fgets(esp);
+        disp(data_esp);
+
+        if contains(data_esp, 'ld')
+            % Assuming the format is 'ld [pos] [id] [timestamp]'
+            dataArray = strsplit(data_esp, ' ');
+            
+            if numel(dataArray) >= 3
+                pos = dataArray{1}(3);
+                id = dataArray{2};
+                timestamp = dataArray{3};
+
+                if contains(id, '1') && length(id)<3
+                    id = 'Lichtschranke';
+                elseif contains(id, '0') && length(id)<3
+                    id = 'ID nicht erkannt';
+                end
+
+               disp(['Pos: ' pos ', ID: ' id ', Timestamp: ' timestamp]);
+
+               receive = ('Aktualisiert...');
+
+                headers = {'Station', 'ID', 'Timestamp'};
+     
+                newData = table({pos}, {id}, {timestamp}, 'VariableNames', headers);
+                
+                    if exist(dataFile, 'file') == 0
+                        % File doesn't exist, create it with headers
+                        writetable(newData, dataFile, 'WriteMode', 'overwrite');
+                    else
+                        % File exists, append data
+                        writetable(newData, dataFile, 'WriteMode', 'append', 'WriteVariableNames', false);
+                    end
+            else
+                disp('Nicht genügend Elemente in gesendet');
+            end
+        else
+            disp('Keine Antwort');
+        end
+        drawnow;
+    end
+end
+
