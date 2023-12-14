374ab983ce72308be7d8bf1bed0ff84ffd92f35c
gerdhinze <gerd.hinteregger@edu.fh-joanneum.at>
2023-12-14 12:54:38 +0100

    -

--- MATLAB/Controll_GUI_V3/untitled.m
+++
@@ -1,5 +0,0 @@
-% Beispiel-Daten erstellen
-data = {'Name', 'Alter', 'Stadt'; 'Alice', 25, 'Berlin'; 'Bob', 30, 'München'; 'Charlie', 22, 'Hamburg'};
-
-% Daten in eine CSV-Datei schreiben
-writecell(data, 'meineDaten.csv');
